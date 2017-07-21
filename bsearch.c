887e728d402d4bbe699eb6a185d341c5b30dae60
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2017-07-20 22:58:07 -0400

    binary search with exclusive upper bound
    binary search using recursion
    
    	modified:   bsearch.c

--- bsearch.c
+++ bsearch.c
@@ -8,8 +8,7 @@
 //#include <stdlib.h>
 #include <stdio.h>
 
-int bsearch(int *table, int sz, int key)
-{
+int bsearch(int *table, int sz, int key) {
 	int low, high, mid;
 
 	if (!table)
@@ -19,25 +18,53 @@ int bsearch(int *table, int sz, int key)
 		return -2;
 	
 	low  = 0;
-	high = sz - 1;
-	mid  = high >> 1;
+	high = sz;
 	while (low < high) {
+		mid = (low + high) >> 1;
+
 		if (key == table[mid])
 			return mid;
 		else if (key < table[mid])
-			high = mid - 1;
+			high = mid;
 		else
 			// key > table[mid]
 			low = mid + 1;
-
-		mid = (low + high) >> 1;
 	}
-	// reach here, we must have low == high
-	if (key == table[mid])
-		return mid;
-
 	return -3;
 }
+
+int bsearch_recur(int *table, int sz, int key) {
+	int mid, ret_val;
+
+	if (!table)
+		return -1;
+
+	if (sz <= 0)
+		return -1;
+
+	if (sz == 1) {
+		if (*table == key)
+			return 0;
+		else
+			return -1;
+	}
+
+	// If we reach here, sz >= 2
+	mid = sz >> 1;
+	if (key == table[mid])
+		return mid;
+	else if (key < table[mid])
+		return bsearch_recur(table, mid, key);
+	else {
+		// key > table[mid]
+		ret_val = bsearch_recur(table + mid + 1, sz - mid - 1, key);
+		if (ret_val < 0)
+			return ret_val;
+		else
+			return mid + 1 + ret_val;
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -46,6 +73,18 @@ int main(int argc, char *argv[])
 	printf("index = %d\n", bsearch(t, sizeof(t)/sizeof(int), 3));
 	printf("index = %d\n", bsearch(t, sizeof(t)/sizeof(int), 9));
 	printf("index = %d\n", bsearch(t, sizeof(t)/sizeof(int), 6));
+
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 1));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 3));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 5));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 7));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 9));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), -1));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 2));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 4));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 6));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 8));
+	printf("index = %d\n", bsearch_recur(t, sizeof(t)/sizeof(int), 10));
 
 	return 0;
 }
